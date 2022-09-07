b49626b07d58d2b50090718cd6d62c135b229bcc
bejoynr <84014121+bejoynr@users.noreply.github.com>
2022-09-07 08:33:10 +0530

    Update fping.sh

--- fping.sh
+++ fping.sh
@@ -3,6 +3,9 @@ echo $LOCALIP
 THIRDOCTET=$(ip -4 -h -o address show wlan0 | awk -F / '{print $1}' | awk '{print $4}'| awk -F. '{print $3}')
 echo "start addr 192.168.$THIRDOCTET.1"
 echo "last addr 192.168.$THIRDOCTET.254"
+echo "Cameras.info file##########"
+cat /etc/health/cameras.info
+echo "###############"
 echo "Hosts Alive"
 fping -s -g 192.168.$THIRDOCTET.1 192.168.$THIRDOCTET.254 -n -a -q -A
 echo "ARP list"
