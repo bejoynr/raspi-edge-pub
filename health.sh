53aebca43b2be9fd858bd9c02cd7654f9435bbaa
bejoynr <84014121+bejoynr@users.noreply.github.com>
2022-11-23 16:36:26 +0530

    Update health.sh

--- health.sh
+++ health.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-uptime -p | awk -F, '{print $1 "" $2}'; printf ", IP:"
+uptime -p | awk -F, '{print $1 "" $2}'|  tr -d '\n'; printf ", IP:"
 curl ifconfig.me ; printf ", "
 cat /dev/null > /tmp/cams.list
-grep -v ^# /etc/health/cameras.info | awk '{print $1}' | while read ip; do curl -o /dev/null -m 5 -s -w "%{http_code}\n" http://$ip > /dev/null 2>&1 && echo $ip IS UP &>> /tmp/cams.list || echo $ip IS DOWN &>> /tmp/cams.list ; done
+grep -v ^# /etc/health/cameras.info | awk '{print $1}' | while read ip; do curl -o /dev/null -s -w "%{http_code}\n" http://$ip > /dev/null 2>&1 && echo $ip IS UP &>> /tmp/cams.list || echo $ip IS DOWN &>> /tmp/cams.list ; done
 printf "Cameras:"
 printf `grep UP /tmp/cams.list| wc -l` && printf '/' && printf `wc -l  /tmp/cams.list`
-ip -4 -h -o address show eth0 | awk -F / '{print $1}' | awk '{print $4}' > /tmp/local.ip
+ip -4 -h -o address show wlan0 | awk -F / '{print $1}' | awk '{print $4}' > /tmp/local.ip
 printf ", Local IP:"
 printf `cat /tmp/local.ip`
 printf ", Arch:"
